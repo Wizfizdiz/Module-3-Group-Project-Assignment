6ca517fa04bf2808ed45f12662383d0e3da30ff8
Wizfizdiz <stripe136@gmail.com>
2025-10-01 00:07:33 -0400

    i have made the userinputs and start up functions carson just has to do the logic and  Guangmei just has to save the results

---
+++ MyProjectPart.py
@@ -0,0 +1,58 @@
+
+
+"""
+carson part implment the following functions below
+"""
+
+def caculate_gross_pay(hours_worked):
+    # make fucntion work
+    # return gros pay
+    pass
+
+
+def caculate_pre_tax_amount(amount):
+    # make fucntion work
+    # return pre tac amount
+    pass
+
+
+def caculate_post_tax_amount(amount):
+    # make fucntion work
+    # return post tax amount
+    pass
+
+
+
+
+employ_first_name = input("What is your first name ? ")
+employ_last_name =  input("what is yout last name ? ")
+employ_id = input("What is your ID ? ")
+employ_number_of_dependent = int(input("What is your number of dependent "))
+employ_hours_worked = int(input("How many hours have your worked "))
+
+gross_pay = caculate_gross_pay(employ_hours_worked)
+
+pre_tax_amount = caculate_pre_tax_amount(gross_pay)
+
+post_tax_amount = caculate_post_tax_amount(gross_pay)
+
+""""
+Guangmei
+
+Save your results into a list, or separate lists/arrays
+Print the results on the screen for the user
+Record your results in a file. This does not have to be done by the program...this can be manual, but would probably be easier just to have the program save it in a file.
+
+"""
+
+results = {
+    
+}
+
+
+
+
+
+
+
+
